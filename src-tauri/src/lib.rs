96430848470f7dcd28aa62f990c6ed090617912e
jakubaszcz <jakub.szczucinski@epitech.eu>
2026-04-20 12:55:08 +0200

    feat(backend): send the song structure to the frontend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,34 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::collections::HashMap;
+
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+pub struct SoundData {
+    pub play: bool,
+    pub path: String
+}
+
+pub type SoundMap = HashMap<String, SoundData>;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_sounds() -> SoundMap {
+    let mut map = SoundMap::new();
+    map.insert(
+        "rain".to_string(),
+        SoundData {
+            play: false,
+            path: "assets/sounds/rain.mp3".to_string(),
+        },
+    );
+    map
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            get_sounds,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
